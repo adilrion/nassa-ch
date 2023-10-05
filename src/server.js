29ed9971587863bb3a46c5d901adbd09d4816560
AdilMahmoudRion <adilmahmoudrion@gmail.com>
2023-10-05 22:03:44 +0600

    s

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 
-const app = express();
+const app = express.default();
 const port = process.env.PORT || 3001;
 
 // Serve static files from the 'dist' folder (assuming Vite build output folder)
