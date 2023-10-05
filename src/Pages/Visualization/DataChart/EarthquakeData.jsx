8111dec053f88dde7ef0d6edf2c3726f922c5ef2
AdilMahmoudRion <adilmahmoudrion@gmail.com>
2023-10-06 00:46:25 +0600

    nnn

--- src/Pages/Visualization/DataChart/EarthquakeData.jsx
+++ src/Pages/Visualization/DataChart/EarthquakeData.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import CanvasJSReact from '@canvasjs/react-charts';
-//var CanvasJSReact = require('@canvasjs/react-charts');
 
-var CanvasJS = CanvasJSReact.CanvasJS;
-var CanvasJSChart = CanvasJSReact.CanvasJSChart;
+const { CanvasJSChart } = CanvasJSReact;
 
 export const EarthquakeData = () => {
     const generateDataPoints = (noOfDps) => {
