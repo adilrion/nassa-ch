ad43ebb292a4cbfa4f7b1d75a60c474a5a0b813b
AdilMahmoudRion <adilmahmoudrion@gmail.com>
2023-10-05 21:33:04 +0600

    s

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,11 @@ import "./App.css";
 import { T } from "./T";
 import { AboutUs } from "./Pages/AboutUs/AboutUs";
 import { Resources } from "./Pages/Resources/Resources";
-import { Visualization } from "./Pages/Visualization/Visualization";
-import { EarthquakeInformation } from "./Pages/EarthqakeInformation/EarthquakeInformation";
-import { DataChart } from "./Pages/Visualization/DataChart/DataChart";
-import { ComparisonChart } from "./Pages/Visualization/ComparisonChart";
-import { TimingAndAlerts } from "./Pages/TimingAndAlerts/TimingAndAlerts";
+// import { Visualization } from "./Pages/Visualization/Visualization";
+// import { EarthquakeInformation } from "./Pages/EarthqakeInformation/EarthquakeInformation";
+// import { DataChart } from "./Pages/Visualization/DataChart/DataChart";
+// import { ComparisonChart } from "./Pages/Visualization/ComparisonChart";
+// import { TimingAndAlerts } from "./Pages/TimingAndAlerts/TimingAndAlerts";
 import { StickyNavbar } from "./components/Navbar/Navbar";
 import { Footer } from "./components/Footer/Footer";
 
@@ -28,11 +28,11 @@ export const App = ()=> {
               {/* <Route path="/home" element={<Home />} /> */}
          <Route path="/about" element={<AboutUs />} />
               <Route path="/resources/documentation" element={<Resources />} />
-              <Route path="/visualization" element={<Visualization />} />
-              <Route path="/visualization/real-time-updates" element={<EarthquakeInformation />} />
-              <Route path="/visualization/data-chart" element={<DataChart />} />
-              <Route path="/visualization/comparison-chart" element={<ComparisonChart />} />
-              <Route path="/visualization/timing-and-alerts" element={<TimingAndAlerts />} />
+              {/* <Route path="/visualization" element={<Visualization />} /> */}
+              {/* <Route path="/visualization/real-time-updates" element={<EarthquakeInformation />} /> */}
+              {/* <Route path="/visualization/data-chart" element={<DataChart />} /> */}
+              {/* <Route path="/visualization/comparison-chart" element={<ComparisonChart />} /> */}
+              {/* <Route path="/visualization/timing-and-alerts" element={<TimingAndAlerts />} /> */}
             </Routes>
           </div>
           <Footer />
