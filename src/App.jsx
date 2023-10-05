d017a9d3cd5492b6d0f0a1b973817e2d0df05f35
AdilMahmoudRion <adilmahmoudrion@gmail.com>
2023-10-05 21:31:08 +0600

    s

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,15 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 
 import { T } from "./T";
+import { AboutUs } from "./Pages/AboutUs/AboutUs";
+import { Resources } from "./Pages/Resources/Resources";
+import { Visualization } from "./Pages/Visualization/Visualization";
+import { EarthquakeInformation } from "./Pages/EarthqakeInformation/EarthquakeInformation";
+import { DataChart } from "./Pages/Visualization/DataChart/DataChart";
+import { ComparisonChart } from "./Pages/Visualization/ComparisonChart";
+import { TimingAndAlerts } from "./Pages/TimingAndAlerts/TimingAndAlerts";
+import { StickyNavbar } from "./components/Navbar/Navbar";
+import { Footer } from "./components/Footer/Footer";
 
 
 
@@ -12,21 +21,21 @@ export const App = ()=> {
       {/* <QueryClientProvider client={queryClient}> */}
         <BrowserRouter>
 
-          {/* <StickyNavbar /> */}
+          <StickyNavbar />
           <div className="">
             <Routes>
               <Route path="/" element={<T />} />
               {/* <Route path="/home" element={<Home />} /> */}
-          {/*     <Route path="/about" element={<AboutUs />} />
+         <Route path="/about" element={<AboutUs />} />
               <Route path="/resources/documentation" element={<Resources />} />
               <Route path="/visualization" element={<Visualization />} />
               <Route path="/visualization/real-time-updates" element={<EarthquakeInformation />} />
               <Route path="/visualization/data-chart" element={<DataChart />} />
               <Route path="/visualization/comparison-chart" element={<ComparisonChart />} />
-              <Route path="/visualization/timing-and-alerts" element={<TimingAndAlerts />} /> */}
+              <Route path="/visualization/timing-and-alerts" element={<TimingAndAlerts />} />
             </Routes>
           </div>
-          {/* <Footer /> */}
+          <Footer />
         </BrowserRouter>
 
       {/* </QueryClientProvider> */}
